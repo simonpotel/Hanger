8d4c674b46684b077c457454c7c2ac2a0635a08c
simon <155122848+simonpotel@users.noreply.github.com>
2024-11-03 03:05:30 +0100

    pygame layers (background 0 players 1 cursor 2)

--- src/client_side/game_client.py
+++ src/client_side/game_client.py
@@ -4,31 +4,39 @@ import threading
 from src.client_side.player_client import PlayerClient
 from loguru import logger
 
+
 class GameClient:
     def __init__(self, width, height, config_path, logo_path):
-        self.width = width # window size
-        self.height = height # window size
-        self.config_path = config_path # path to the config file configs/host.json
-        self.logo_path = logo_path # logo of the game
-        self.screen = None # pygame screen obj
-        self.font = None # pygame font obj
-        self.client_logo = None # pygame image obj
-        self.clock = None # pygame clock obj
-        self.client = None # PlayerClient obj
-        self.running = True 
-        logger.info("GameClient initialized with width: {}, height: {}, config_path: {}, logo_path: {}", width, height, config_path, logo_path)
+        self.width = width  # window size
+        self.height = height  # window size
+        self.config_path = config_path  # path to the config file configs/host.json
+        self.logo_path = logo_path  # logo of the game
+        self.screen = None  # pygame screen obj
+        self.font = None  # pygame font obj
+        self.client_logo = None  # pygame image obj
+        self.clock = None  # pygame clock obj
+        self.client = None  # PlayerClient obj
+        self.running = True
+        logger.info("GameClient initialized with width: {}, height: {}, config_path: {}, logo_path: {}",
+                    width, height, config_path, logo_path)
 
     def setup_screen(self):
-        self.screen = pygame.display.set_mode((self.width, self.height)) # create the game window
-        pygame.display.set_caption("Hanger Client") # set the game window title
+        self.screen = pygame.display.set_mode(
+            (self.width, self.height))  # create the game window
+        # set the game window title
+        pygame.display.set_caption("Hanger Client")
         pygame.mouse.set_visible(False)  # hide the default mouse cursor
-        logger.info("Screen setup with width: {}, height: {}", self.width, self.height)
+        logger.info("Screen setup with width: {}, height: {}",
+                    self.width, self.height)
 
     def load_assets(self):
-        self.font = pygame.font.Font(None, 24) # load the font for the game
-        self.client_logo = pygame.image.load(self.logo_path) # load the logo of the game
-        pygame.display.set_icon(self.client_logo) # set the logo of the game    
-        self.client_logo = pygame.transform.scale(self.client_logo, (128, 128)) # scale the logo if needed to 128x128
+        self.font = pygame.font.Font(None, 24)  # load the font for the game
+        self.client_logo = pygame.image.load(
+            self.logo_path)  # load the logo of the game
+        pygame.display.set_icon(self.client_logo)  # set the logo of the game
+        self.client_logo = pygame.transform.scale(
+            # scale the logo if needed to 128x128
+            self.client_logo, (128, 128))
         logger.info("Assets loaded successfully")
 
     def read_config(self):
@@ -37,14 +45,17 @@ class GameClient:
             with open(self.config_path, 'r') as f:
                 config = json.load(f)
             logger.info("Config read successfully from {}", self.config_path)
-            return config.get('ip'), config.get('port') # return the ip and port of the server
+            # return the ip and port of the server
+            return config.get('ip'), config.get('port')
         except Exception as e:
             logger.error("Failed to read config: {}", e)
             raise
 
     def start_client(self, ip, port):
         self.client = PlayerClient(ip=ip, port=port)
-        threading.Thread(target=self.client.receive_updates, daemon=True).start() # start the thread to receive updates from the server 
+        # start the thread to receive updates from the server
+        threading.Thread(target=self.client.receive_updates,
+                         daemon=True).start()
         logger.info("Client started with IP: {}, Port: {}", ip, port)
 
     def handle_events(self):
@@ -96,45 +107,57 @@ class GameClient:
 
     def draw(self):
         # draw the entites of the game (using the tabs updated by handlers with server)
-        self.screen.fill((255, 255, 255)) # fill the screen with white color (reset the screen)
+        # fill the screen with white color (reset the screen)
+        if self.client.player is None:
+            return
+        self.screen.fill((255, 255, 255))
         n_players = list(self.client.players.values())
 
-        if self.client.player is not None:
-            # my player exists
-            # manage the camera offset to follow the player (ur own) in center of the game window
-            camera_offset_x = self.client.player.position[0] - self.width // 2 # camera offset x center
-            camera_offset_y = self.client.player.position[1] - self.height // 2  # camera offset y center
-
-            for player in n_players: # do this for every players in the game
-                if player.id == self.client.player_id:
-                    self.client.player = player  # update the player in the client side if needed in other part of the code
-                extra = True
-                if self.client.player is not None:
-                    if (abs(player.position[0] - self.client.player.position[0])**2 + abs(player.position[1] - self.client.player.position[1])**2)**0.5 > 500:
-                        extra = False  # distance between the player and the other player is > 500 so we don't draw the other player extra infos
-                
-                draw_position = (player.position[0] - camera_offset_x, player.position[1] - camera_offset_y) # position of the player on the screen (centered)
-                # draw the player in the screen
-                player.draw(self.screen, self.font, (0, 0, 0), extra, draw_position)  # write the player on the screen with the correct position (with offset)
-        if pygame.mouse.get_focused(): # if the mouse is focused on the game window
-            cursor_image = pygame.image.load('assets/Tiny Swords/UI/Pointers/01.png') # load the cursor image
-            cursor_image = pygame.transform.scale(cursor_image, (64, 64))  # scale the cursor image 
-            mouse_x, mouse_y = pygame.mouse.get_pos() # get the position of the mouse on the screen
-            self.screen.blit(cursor_image, (mouse_x - cursor_image.get_width() // 2, mouse_y - cursor_image.get_height() // 2)) # draw the cursor on the screen
+        # manage the camera offset to follow the player (ur own) in center of the game window
+        # camera offset x center
+        camera_offset_x = self.client.player.position[0] - self.width // 2
+        # camera offset y center
+        camera_offset_y = self.client.player.position[1] - self.height // 2
 
         if 'world' in self.client.maps.maps:
             for asset, properties in self.client.maps.maps['world'].items():
                 asset_image = pygame.image.load(properties['asset_path'])
-                asset_image = pygame.transform.scale(asset_image, (properties['width'], properties['height']))
+                asset_image = pygame.transform.scale(
+                    asset_image, (properties['width'], properties['height']))
                 self.screen.blit(
                     asset_image,
                     (properties['x'] - camera_offset_x - properties['width'] // 2,
                     properties['y'] - camera_offset_y - properties['height'] // 2)
                 )
+        
+        for player in n_players:  # do this for every players in the game
+            if player.id == self.client.player_id:
+                # update the player in the client side if needed in other part of the code
+                self.client.player = player
+            extra = True
+            if self.client.player is not None:
+                if (abs(player.position[0] - self.client.player.position[0])**2 + abs(player.position[1] - self.client.player.position[1])**2)**0.5 > 500:
+                    extra = False  # distance between the player and the other player is > 500 so we don't draw the other player extra infos
+            # position of the player on the screen (centered)
+            draw_position = (
+                player.position[0] - camera_offset_x, player.position[1] - camera_offset_y)
+            # draw the player in the screen
+            # write the player on the screen with the correct position (with offset)
+            player.draw(self.screen, self.font,
+                        (0, 0, 0), extra, draw_position)
+
+        if pygame.mouse.get_focused():  # if the mouse is focused on the game window
+            cursor_image = pygame.image.load(
+                'assets/Tiny Swords/UI/Pointers/01.png')  # load the cursor image
+            cursor_image = pygame.transform.scale(
+                cursor_image, (64, 64))  # scale the cursor image
+            # get the position of the mouse on the screen
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            self.screen.blit(cursor_image, (mouse_x - cursor_image.get_width() // 2,
+                             mouse_y - cursor_image.get_height() // 2))  # draw the cursor on the screen
 
         pygame.display.flip()  # update the screen
         logger.debug("Screen drawn with {} players", len(n_players))
-    
 
     def run(self):
         # run the game
@@ -143,15 +166,16 @@ class GameClient:
         self.setup_screen()
         self.load_assets()
         self.clock = pygame.time.Clock()
-        ip, port = self.read_config() 
+        ip, port = self.read_config()
         self.start_client(ip, port)
 
         while self.running:
-            dt = self.clock.tick(120) / 1000 # get the delta time between each frame (to have a smooth game)
-            self.handle_events() # handle the events of the game
-            self.update(dt) # update the game
-            self.draw() # draw the game
+            # get the delta time between each frame (to have a smooth game)
+            dt = self.clock.tick(120) / 1000
+            self.handle_events()  # handle the events of the game
+            self.update(dt)  # update the game
+            self.draw()  # draw the game
 
-        self.client.close() # close the connection with the server
-        pygame.quit() # quit the game
+        self.client.close()  # close the connection with the server
+        pygame.quit()  # quit the game
         logger.info("Game closed")
